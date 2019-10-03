186c498f2f01191698873fbb64ecab90904e11de
Pignataro67 <Pignataro67@gmail.com>
2019-10-03 14:15:52 -0700

    adding results comp.

---
+++ client/components/Results.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Result from './Result';
+import Card from '../Card';
+
+class Results extends Commponent {
+  render() {
+    return (
+      <div>
+        <Card label={this.props.label}>
+          <Result header={"Uber"} uberEstimates={this.props.uberEstimates}/>
+          <Result header={"Lyft"} lyftEstimates={this.props.lyftEstimates}/>
+        </Card>
+      </div>
+    )
+  }
+}
+
+export default Results;
