b93e8bed34d97ad6dd236ca84454265030f8052b
Pignataro67 <Pignataro67@gmail.com>
2019-10-05 06:13:47 -0700

    adding state and redirect func.

--- client/components/confirmRoute/confirmRoute.js
+++ client/components/confirmRoute/confirmRoute.js
@@ -6,12 +6,26 @@ import { Redirect } from "react-router-dom";
 
 class ConfirmRoute extends Component{
 
-   render(){
-  
+  state = {
+    redirectToResults: false,
+    redirectToSearch: false
+  }
+
+  render(){
+    const { redirectToResults } = this.state;
+    const { redirectToSearch } = this.state;
+      if(redirectToResults) {
+        return <Redirect to="/results"/>
+    } else if (redirectToSearch) {
+      return <Redirect to='/'/>
+    }
+
   return(
     <div>
       <Card>
         <RouteMap mapboxKey={this.props.mapboxKey} startLat={this.props.startLat} startLng={this.props.startLng} destinationLat={this.props.destinationLat} destinationLng={this.props.destinationLng}/>
+        <Button buttonTitle="No, let's try that again." onClick={this.handleRedirectBack}/> 
+        <Button buttonTitle="Yes! Let's Ride..." onClick={this.handleRedirect}/>
       </Card>
     </div>
     )
