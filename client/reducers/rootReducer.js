165e5640c2ab77ea2f2615b8f63b542706f530d3
Pignataro67 <Pignataro67@gmail.com>
2019-10-07 12:47:29 -0700

    adding mo reducers

--- client/reducers/rootReducer.js
+++ client/reducers/rootReducer.js
@@ -5,6 +5,16 @@ export default function rootReducer(state = {
   isFetchingDropOff: false,
   suggestedPickupLocations: [],
   suggestedDropOff: [],
+  isConvertingPickupLatLong: false,
+  isConvertingDropoffLatLong: false,
+  pickupLat: '',
+  pickupLong: '',
+  dropoffLat: '',
+  dropoffLong: '',
+  isFetchingUberEstimate: false,
+  isFetchingLyftEstimate: false,
+  uberEstimates: '',
+  lyftEstimates: '',
     }, action) {
     switch(action.type){
       case "FETCHING_MAPBOX_KEY":
@@ -19,6 +29,22 @@ export default function rootReducer(state = {
         return {...state, isFetchingDropOff: true, suggestedDropOffs: []}
       case "DISPLAY_DROPOFFS":
         return {...state, isFetchingDropOff: false, suggestedDropOffs: action.suggestedDropOffs}
+        case "CONVERTING_PICKUP_LAT_LONG":
+        return {...state, isConvertingPickupLatLong: true}
+      case "RETRIEVE_PICKUP_LAT_LONG":
+        return {...state, isConvertingPickupLatLong: false, pickupLatLong: action.pickupLatLong}
+      case "CONVERTING_DROPOFF_LAT_LONG":
+        return {...state, isConvertingDropoffLatLong: true}
+      case "RETRIEVE_DROPOFF_LAT_LONG":
+        return {...state, isConvertingDropoffLatLong: false, dropoffLatLong: action.dropoffLatLong}
+      case "RETURN_UBER_ESTIMATE":
+        return {...state, isFetchingUberEstimate: true, uberEstimates: ''}
+      case "ADD_UBER_ESTIMATE_TO_STATE":
+        return {...state, isFetchingUberEstimate: false, uberestimates: action.estimates }
+      case "FETCHING_LYFT_ESTIMATE":
+        return {...state, isFetchingLyftEstimate: true, lyftEstimates: ''}
+      case "ADD_LYFT_ESTIMATES_TO_STATE":
+        return {...state, isFetchingLyftEstimate: false, lyftEstimates: action.estimates }
       default: 
         return state
     }
